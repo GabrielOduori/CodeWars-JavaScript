f1af691dd8cb51b9288759469019f7f6734ab15a
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-10 21:41:25 +0300

    Added howMuchILoveYou

---
+++ howMuchILoveYou.js
@@ -0,0 +1,10 @@
+function howMuchILoveYou(nbPetals) {
+ let whatToSay = ['I love you','a little','a lot','passionately','madly','not at all'];
+ while (nbPetals>0){
+   if(nbPetals<=whatToSay.length){
+     return whatToSay[(nbPetals-1)];
+   } else {
+     return whatToSay[(nbPetals-1)%6];
+   }
+ }
+}
